beb5d4f88c50973d7d24ece8de3ffcf8c98cf703
Soedjamal <akmalihksan181@gmail.com>
2025-03-18 14:22:38 +0700

    add: fetch product

--- src/components/custom/ProductSection.tsx
+++ src/components/custom/ProductSection.tsx
@@ -1,24 +1,38 @@
+import { axiosInstance } from "@/lib/axios";
 import ProductCard from "./ProductCard";
+import { useQuery } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
 
-const productData = [
-  {
-    id: 1,
-    name: "Dimsum",
-    price: 10000,
-  },
-  {
-    id: 2,
-    name: "Dimsum",
-    price: 20000,
-  },
-];
+// type ProductType = {
+//   _id?: number;
+//   name: string;
+//   price: number;
+// };
 
 const ProductSection = () => {
+  // const [productData, setProductData] = useState([]);
+
+  const getProducts: any = async () => {
+    const response = await axiosInstance("/api/products");
+    return response.data;
+  };
+
+  // useEffect(() => {
+  //   getProducts();
+  // }, []);
+
+  const { data: productData } = useQuery({
+    queryFn: getProducts,
+    queryKey: ["products"],
+  });
+
+  console.log(productData);
+
   return (
     <>
       <div className="flex w-full">
         <div className="product-scrollX flex w-full gap-x-4 overflow-x-auto">
-          {productData.map((product, index) => (
+          {productData.map((product: any, index: any) => (
             <ProductCard
               id={product.id}
               name={product.name}
